1ff60dfb59eba6a0753d0fe20b5fecc3a91e2ac7
Adrian31BT <adrian31baciliot@gmail.com>
2024-11-08 16:04:27 -0500

    reto9: implementado el servicio para crear Proveedor

--- java/inventarios/src/main/java/com/krakedev/inventarios/servicios/ServiciosProveedores.java
+++ java/inventarios/src/main/java/com/krakedev/inventarios/servicios/ServiciosProveedores.java
@@ -2,7 +2,9 @@ package com.krakedev.inventarios.servicios;
 
 import java.util.ArrayList;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,8 +14,6 @@ import javax.ws.rs.core.Response;
 import com.krakedev.inventarios.bdd.ProveedoresBDD;
 import com.krakedev.inventarios.entidades.Proveedor;
 import com.krakedev.inventarios.excepciones.KrakeDevException;
-
-
 
 @Path("proveedores")
 public class ServiciosProveedores {
@@ -30,5 +30,19 @@ public class ServiciosProveedores {
 			e.printStackTrace();
 			return Response.serverError().build();
 		}	
+	}
+	
+	@Path("crear")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response crear(Proveedor proveedor) {
+		ProveedoresBDD proveedoresBDD = new ProveedoresBDD();
+		try {
+			proveedoresBDD.insertar(proveedor);
+			return Response.ok().build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
 	}
 }
