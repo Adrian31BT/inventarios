d923b82048a1c5c3c841deef2ed560a1e0b61ea7
Adrian31BT <adrian31baciliot@gmail.com>
2024-11-25 19:52:22 -0500

    reto28: Se implementaron cambios en Productos, se agrego el TouchableOpacity al editar y al eliminar mostrar un modal primero

--- reactnavite/Productos/App.js
+++ reactnavite/Productos/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, Text, View, TextInput, Button, Alert, ScrollView } from 'react-native';
+import { FlatList, StyleSheet, Text, View, TextInput, Button, Alert, ScrollView, TouchableHighlight, Modal, Pressable } from 'react-native';
 import { useState } from 'react';
+
 
 let productos = [
   { nombre: "Doritos", categoria: "Snacks", precioCompra: "0.40", precioVenta: "0.45", id: "100" },
@@ -20,33 +21,34 @@ export default function App() {
   const [txtCategoria, setTxtCategoria] = useState();
   const [txtPrecioCompra, setTxtPrecioCompra] = useState();
   const [txtPrecioVenta, setTxtPrecioVenta] = useState();
-  const [numElementos, setNumElementos ] = useState(productos.length);
+  const [numElementos, setNumElementos] = useState(productos.length);
+  const [modalVisible, setModalVisible] = useState(false);
 
-  let existeProducto = () =>{
-    for(let i = 0; i < productos.length; i++){
-      if(productos[i].id == txtId){
+  let existeProducto = () => {
+    for (let i = 0; i < productos.length; i++) {
+      if (productos[i].id == txtId) {
         return true;
       }
     }
     return false;
   }
 
-  let camposVacios = () =>{
-    if(txtId==null || txtNombre==null || txtCategoria==null || txtPrecioCompra==null ||txtId=="" || txtNombre=="" || txtCategoria=="" || txtPrecioCompra==""){
+  let camposVacios = () => {
+    if (txtId == null || txtNombre == null || txtCategoria == null || txtPrecioCompra == null || txtId == "" || txtNombre == "" || txtCategoria == "" || txtPrecioCompra == "") {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
 
   let guardarProducto = () => {
-    if(camposVacios()){
+    if (camposVacios()) {
       Alert.alert("INFO", "Ingrese todos los campos obligatorios");
-    }else{
-      if(esNuevo){
-        if(existeProducto()){
+    } else {
+      if (esNuevo) {
+        if (existeProducto()) {
           Alert.alert("INFO", "Ya existe un producto con el id");
-        }else{
+        } else {
           let floatPrecioCompra = parseFloat(txtPrecioCompra);
           let precioVentaCalculado = floatPrecioCompra + (floatPrecioCompra * 0.20);
           precioVentaCalculado = precioVentaCalculado.toFixed(2);
@@ -59,60 +61,64 @@ export default function App() {
           };
           productos.push(producto);
         }
-      }else{
+      } else {
         productos[indiceSeleccionado].nombre = txtNombre;
         productos[indiceSeleccionado].categoria = txtCategoria;
         productos[indiceSeleccionado].precioCompra = txtPrecioCompra;
         let floatPrecioCompra = parseFloat(productos[indiceSeleccionado].precioCompra);
         let precioVentaCalculado = floatPrecioCompra + (floatPrecioCompra * 0.20);
-        productos[indiceSeleccionado].precioVenta = precioVentaCalculado.toFixed(2); 
+        productos[indiceSeleccionado].precioVenta = precioVentaCalculado.toFixed(2);
       }
       limpiar();
       setNumElementos(productos.length);
     }
   }
 
-  let ItemProducto = (props) => {
+  let ItemProducto = ({ indice, producto }) => {
     return (
-      <View style={styles.producto}>
-        <View style={styles.itemIndice}>
-          <Text style={styles.texto1}>
-            {props.producto.id}
-          </Text>
+      <TouchableHighlight
+        activeOpacity={0.6}
+        underlayColor="#6592d0"
+        onPress={() => {
+          setTxtId(producto.id);
+          setTxtNombre(producto.nombre);
+          setTxtCategoria(producto.categoria);
+          setTxtPrecioCompra(producto.precioCompra);
+          setTxtPrecioVenta(producto.precioVenta);
+          esNuevo = false;
+          indiceSeleccionado = indice;
+        }}>
+
+        <View style={styles.producto}>
+          <View style={styles.itemIndice}>
+            <Text style={styles.texto1}>
+              {producto.id}
+            </Text>
+          </View>
+          <View style={styles.itemContenido}>
+            <Text style={styles.texto1}>
+              {producto.nombre} ({producto.categoria})
+            </Text>
+            <Text style={styles.texto2}>
+              USD {producto.precioVenta}
+            </Text>
+          </View>
+          <View style={styles.itemBotones}>
+            <Button
+              title=" X "
+              color="red"
+              onPress={() => {
+                setModalVisible(true);
+                indiceSeleccionado = indice;
+                /* indiceSeleccionado = indice;
+                productos.splice(indiceSeleccionado, 1);
+                setNumElementos(productos.length); */
+              }}
+            />
+          </View>
         </View>
-        <View style={styles.itemContenido}>
-          <Text style={styles.texto1}>
-            {props.producto.nombre} ({props.producto.categoria})
-          </Text>
-          <Text style={styles.texto2}>
-            USD {props.producto.precioVenta}
-          </Text>
-        </View>
-        <View style={styles.itemBotones}>
-          <Button
-            title=" E "
-            color="green"
-            onPress={() => {
-              setTxtId(props.producto.id);
-              setTxtNombre(props.producto.nombre);
-              setTxtCategoria(props.producto.categoria);
-              setTxtPrecioCompra(props.producto.precioCompra);
-              setTxtPrecioVenta(props.producto.precioVenta); 
-              esNuevo = false;
-              indiceSeleccionado = props.indice;
-            }}
-          />
-          <Button
-            title=" X "
-            color="red"
-            onPress={() => {
-              indiceSeleccionado = props.indice;
-              productos.splice(indiceSeleccionado, 1);
-              setNumElementos(productos.length);
-            }}
-          />
-        </View>
-      </View>
+      </TouchableHighlight>
+
     )
   }
 
@@ -126,67 +132,95 @@ export default function App() {
   }
 
   return (
+
     <View style={styles.container}>
-      <ScrollView> 
-      <View style={styles.areaCabecera}>
-        <Text style={styles.titulo}>PRODUCTOS</Text>
-        <TextInput style={styles.txt}
-          value={txtId}
-          placeholder='Ingrese id'
-          onChangeText={setTxtId}
-          keyboardType='numeric'
-          editable={esNuevo}
-        />
-        <TextInput style={styles.txt}
-          value={txtNombre}
-          placeholder='Ingrese nombre'
-          onChangeText={setTxtNombre}
-        />
-        <TextInput style={styles.txt}
-          value={txtCategoria}
-          placeholder='Ingrese categoría'
-          onChangeText={setTxtCategoria}
-        />
-        <TextInput style={styles.txt}
-          value={txtPrecioCompra}
-          placeholder='Ingrese precio de compra'
-          onChangeText={setTxtPrecioCompra}
-          keyboardType='numeric'
-        />
-        <TextInput style={styles.txt}
-          value={txtPrecioVenta}
-          placeholder='Precio de venta'
-          onChangeText={setTxtPrecioVenta}
-          keyboardType='numeric'
-          editable={false}
-        />
-        <View style={styles.areaBotones}>
-          <Button
-            title="Guardar"
-            onPress={() => {
-              guardarProducto();
-            }}
-          />
-          <Button
-            title="Nuevo"
-            onPress={() => {
-              limpiar();
-            }}
-          />
-          <Text>Elementos: {numElementos}</Text>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View style={styles.areaCabeceraModal}>
+              <Text style={styles.modalText}>¿Está seguro que quiere eliminar?</Text>
+            </View>
+            <View style={styles.areaBotonesModal}>
+              <Pressable
+                style={[styles.button, styles.buttonAceptar]}
+                onPress={() => {
+                  productos.splice(indiceSeleccionado, 1);
+                  setNumElementos(productos.length);
+                  setModalVisible(!modalVisible);
+                }}>
+                <Text style={styles.textStyle}>Aceptar</Text>
+              </Pressable>
+              <Pressable
+                style={[styles.button, styles.buttonCancelar]}
+                onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.textStyle}>Cancelar</Text>
+              </Pressable>
+            </View>
+          </View>
         </View>
-        
-      </View>
-      </ScrollView> 
+      </Modal>
+      <ScrollView>
+        <View style={styles.areaCabecera}>
+          <Text style={styles.titulo}>PRODUCTOS</Text>
+          <TextInput style={styles.txt}
+            value={txtId}
+            placeholder='Ingrese id'
+            onChangeText={setTxtId}
+            keyboardType='numeric'
+            editable={esNuevo}
+          />
+          <TextInput style={styles.txt}
+            value={txtNombre}
+            placeholder='Ingrese nombre'
+            onChangeText={setTxtNombre}
+          />
+          <TextInput style={styles.txt}
+            value={txtCategoria}
+            placeholder='Ingrese categoría'
+            onChangeText={setTxtCategoria}
+          />
+          <TextInput style={styles.txt}
+            value={txtPrecioCompra}
+            placeholder='Ingrese precio de compra'
+            onChangeText={setTxtPrecioCompra}
+            keyboardType='numeric'
+          />
+          <TextInput style={styles.txt}
+            value={txtPrecioVenta}
+            placeholder='Precio de venta'
+            onChangeText={setTxtPrecioVenta}
+            keyboardType='numeric'
+            editable={false}
+          />
+          <View style={styles.areaBotones}>
+            <Button
+              title="Guardar"
+              onPress={() => {
+                guardarProducto();
+              }}
+            />
+            <Button
+              title="Nuevo"
+              onPress={() => {
+                limpiar();
+              }}
+            />
+            <Text>Elementos: {numElementos}</Text>
+          </View>
+
+        </View>
+      </ScrollView>
       <View style={styles.areaContenido}>
         <FlatList
           data={productos}
-          renderItem={(elemento) => {
-            return <ItemProducto indice={elemento.index} producto={elemento.item} />
+          renderItem={({ index, item }) => {
+            return <ItemProducto indice={index} producto={item} />
           }}
-          keyExtractor={(item) => {
-            return item.id
-          }}
+          keyExtractor={item => item.id}
         />
       </View>
       <View style={styles.areaPie}>
@@ -256,9 +290,9 @@ const styles = StyleSheet.create({
   },
   itemBotones: {
     flexDirection: "row",
-    flex: 2,
+    flex: 1,
     alignItems: "center",
-    justifyContent: "space-between"
+    justifyContent: "center"
   },
   txt: {
     borderWidth: 1,
@@ -271,5 +305,50 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-evenly",
     marginTop: 5
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  areaBotonesModal: {
+    flexDirection: "row",
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonAceptar: {
+    backgroundColor: '#5cf523',
+    marginHorizontal: 10
+  },
+  buttonCancelar: {
+    backgroundColor: '#f53323',
+    marginHorizontal: 10
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   }
 });
