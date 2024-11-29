2c411033d59612e3f2788c1f60bcddbfae401850
Adrian31BT <adrian31baciliot@gmail.com>
2024-11-28 20:38:16 -0500

    reto30: Implementado los cambios de navegacion por Stack, Drawer y Tab en el proyecto Notas

--- reactnavite/Notas/App.js
+++ reactnavite/Notas/App.js
@@ -4,17 +4,95 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { GradeForm } from './app/screens/GradeForm';
 import { ListGrades } from './app/screens/ListGrades';
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ContenidoA } from './app/screens/ContenidoA';
+import { ContenidoB } from './app/screens/ContenidoB';
+import { Icon } from '@rneui/base';
 
 export default function App() {
 
-  const StackGrades = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
+  const Drawer = createDrawerNavigator();
+  const Tab = createBottomTabNavigator();
+
+  const TabNav = () =>{
+    return(
+      <Tab.Navigator>
+        <Tab.Screen
+          name='TabContenidoA'
+          component={ContenidoA}
+          options={{
+            headerShown: false,
+            tabBarLabel: "Configuracion",
+            tabBarIcon:({size, tintColor})=>{
+              return <Icon name='user' size={size} color={tintColor} type='ant-design'/>
+            }
+          }}
+        />
+        <Tab.Screen
+          name='TabContenidoB'
+          component={ContenidoB}
+          options={{
+            headerShown: false,
+            tabBarLabel: "Acerca De",
+            tabBarIcon:({size, tintColor})=>{
+              return <Icon name='mail' size={size} color={tintColor} type='ant-design'/>
+            }
+          }}
+        />
+      </Tab.Navigator>
+    )
+  }
+  
+  const DrawerNav=()=>{
+    return(
+      <Drawer.Navigator>
+        <Drawer.Screen
+          name='ListGradesNav'
+          component={ListGrades}
+          options={{
+            title:"Navegacion Draw"
+          }}
+        />
+        <Drawer.Screen
+          name='ListGrades'
+          component={StackNav}
+          options={{
+            title:"Navegacion Stack"
+          }}
+        />
+        <Drawer.Screen
+          name='TabContenidoA'
+          component={TabNav}
+          options={{
+            title:"Navegacion Tab"
+          }}
+        />
+      </Drawer.Navigator>
+    )
+  }
+ 
+  const StackNav=()=>{
+    return(
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="ListGradesNav" 
+          component={ListGrades}
+        />
+        <Stack.Screen 
+          name="GradeFormNav" 
+          component={GradeForm}
+        />
+      </Stack.Navigator>
+    )
+  }
 
   return (
     <NavigationContainer>
-      <StackGrades.Navigator>
-      <StackGrades.Screen name="ListGradesNav" component={ListGrades} />
-        <StackGrades.Screen name="GradeFormNav" component={GradeForm} />
-      </StackGrades.Navigator>
+      <DrawerNav/> 
     </NavigationContainer>
   );
+  
 }
